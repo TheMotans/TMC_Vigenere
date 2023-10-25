461ab9aec9fe66dd5bf72f21fa9f62411817e5ed
TheMotans <stoica.adrian1997@gmail.com>
2023-10-25 13:16:58 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBoxMessage = new TextBox();
+            label1 = new Label();
+            textBoxKey = new TextBox();
+            label2 = new Label();
+            button1 = new Button();
+            labelEncrypt = new Label();
+            button2 = new Button();
+            button3 = new Button();
+            SuspendLayout();
+            // 
+            // textBoxMessage
+            // 
+            textBoxMessage.Location = new Point(157, 34);
+            textBoxMessage.Name = "textBoxMessage";
+            textBoxMessage.Size = new Size(267, 23);
+            textBoxMessage.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(92, 37);
+            label1.Name = "label1";
+            label1.Size = new Size(53, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Message";
+            // 
+            // textBoxKey
+            // 
+            textBoxKey.Location = new Point(157, 76);
+            textBoxKey.Name = "textBoxKey";
+            textBoxKey.Size = new Size(267, 23);
+            textBoxKey.TabIndex = 0;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(92, 79);
+            label2.Name = "label2";
+            label2.Size = new Size(26, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Key";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(439, 34);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 3;
+            button1.Text = "Encrypt";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // labelEncrypt
+            // 
+            labelEncrypt.Location = new Point(93, 122);
+            labelEncrypt.Name = "labelEncrypt";
+            labelEncrypt.Size = new Size(331, 45);
+            labelEncrypt.TabIndex = 4;
+            labelEncrypt.Text = "label3";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(439, 76);
+            button2.Name = "button2";
+            button2.Size = new Size(145, 23);
+            button2.TabIndex = 3;
+            button2.Text = "Decrypt_ForceBrute";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(520, 34);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 5;
+            button3.Text = "Decrypt";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1055, 600);
+            Controls.Add(button3);
+            Controls.Add(labelEncrypt);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(textBoxKey);
+            Controls.Add(label1);
+            Controls.Add(textBoxMessage);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBoxMessage;
+        private Label label1;
+        private TextBox textBoxKey;
+        private Label label2;
+        private Button button1;
+        private Label labelEncrypt;
+        private Button button2;
+        private Button button3;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,171 @@
+﻿
+using Microsoft.VisualBasic.Devices;
+
+namespace Test
+{
+    public partial class Form1 : Form
+    {
+        static string[] alphabet { get; set; } = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+
+        public Form1()
+        {
+            InitializeComponent();
+            //string encryptedText = Encrypt("ABC", "BBB");
+            //labelEncrypt.Text = encryptedText;
+            //string decrypttext = DecryptVigenere("BCD", "BBB");
+            //textBoxMessage.Text = decrypttext;
+            KasiskiTest();
+        }
+        static string Encrypt(string plaintext, string keyword)
+        {
+            string encryptedText = "";
+
+            for (int i = 0, j = 0; i < plaintext.Length; i++)
+            {
+                string c = plaintext[i].ToString().ToUpper();
+                string a = keyword[j % keyword.Length].ToString().ToUpper();
+                if (alphabet.Contains(c))
+                {
+                    int position = alphabet.ToList().IndexOf(c);
+                    int key = alphabet.ToList().IndexOf(a);
+                    int sum = (position + key) % 26;
+                    encryptedText += alphabet[sum];
+                    j++;
+                }
+                else
+                {
+                    encryptedText += c;
+                }
+            }
+            return encryptedText;
+        }
+
+        static string DecryptVigenere(string ciphertext, string key)
+        {
+            string plaintext = "";
+
+            for (int i = 0, j = 0; i < ciphertext.Length; i++)
+            {
+                string c = ciphertext[i].ToString().ToUpper();
+                string a = key[j % key.Length].ToString().ToUpper();
+
+                if (alphabet.Contains(c))
+                {
+                    int position = alphabet.ToList().IndexOf(c);
+                    int shift = alphabet.ToList().IndexOf(a);
+                    plaintext += alphabet[(position - shift + 26) % 26];
+                    j++;
+                }
+                else
+                {
+                    plaintext += c;
+                }
+            }
+            return plaintext;
+        }
+
+
+        static void KasiskiTest()
+        {
+            string ciphertext = "JFNHCQZXKRLBPGFISWYJFNHCQZXKRLBPGFISW";
+
+            int minRepeatDistance = 1; // Lungimea minimă a secvențelor repetate
+            Dictionary<string, List<int>> repeatedSequences = new Dictionary<string, List<int>>();
+
+            // Căutare secvențe repetate
+            for (int i = 0; i < ciphertext.Length - minRepeatDistance; i++)
+            {
+                for (int j = i + minRepeatDistance; j < ciphertext.Length; j++)
+                {
+                    string sequence = ciphertext.Substring(i, j - i);
+
+                    if (ciphertext.Substring(j).Contains(sequence))
+                    {
+                        if (!repeatedSequences.ContainsKey(sequence))
+                            repeatedSequences[sequence] = new List<int>();
+
+                        repeatedSequences[sequence].Add(j - i);
+                    }
+                }
+            }
+
+            // Identificarea factorilor comuni
+            var commonFactors = new List<int>();
+            foreach (var sequence in repeatedSequences)
+            {
+                List<int> distances = sequence.Value;
+                if (distances.Count >= 2)
+                {
+                    for (int i = 0; i < distances.Count - 1; i++)
+                    {
+                        for (int j = i + 1; j < distances.Count; j++)
+                        {
+                            int factor = GCD(distances[i], distances[j]);
+                            commonFactors.Add(factor);
+                        }
+                    }
+                }
+            }
+
+            // Afișarea factorilor comuni
+            Console.WriteLine("Factori comuni:");
+            foreach (int factor in commonFactors.Distinct())
+            {
+                Console.WriteLine(factor);
+            }
+        }
+
+        // Funcție pentru calculul celui mai mare divizor comun
+        static int GCD(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+    
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string plaintext = textBoxMessage.Text;
+            string keyword = textBoxKey.Text; // Your keyword here
+            string encryptedText = Encrypt(plaintext, keyword);
+
+            labelEncrypt.Text = encryptedText;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string ciphertext = labelEncrypt.Text; // Textul cifrat
+            int keyLength = textBoxKey.TextLength; // Lungimea cheii (ar trebui să fie cunoscută)
+
+            for (int i = 0; i < char.MaxValue; i++)
+            {
+                for (int j = 0; j < char.MaxValue; j++)
+                {
+                    for (int k = 0; k < char.MaxValue; k++)
+                    {
+                        string key = $"{(char)i}{(char)j}{(char)k}";
+                        string plaintext = DecryptVigenere(ciphertext, key);
+                        textBoxMessage.Text = plaintext;
+                        textBoxKey.Text = key;
+                    }
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string ciphertext = labelEncrypt.Text;
+            string keyword = textBoxKey.Text; // Your keyword here
+            string decrypttext = DecryptVigenere(ciphertext, keyword);
+
+            textBoxMessage.Text = decrypttext;
+        }
+    }
+}
